ae4132ab84f61b94868f39c9e0c34fdb0d485fbf
yu9824 <58211916+yu9824@users.noreply.github.com>
2024-05-11 15:50:17 +0900

    Update conf.py

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -6,6 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import sys
+
+sys.path.insert(0, "../src")
+
 from template import __author__, __version__
 
 project = "template"
